f17526e50b7f0734117f2422b0ed419453d3d908
robbibt <robbibt@gmail.com>
2016-02-26 17:50:33 +1100

    Fixed non-working plotting

--- scouting_camera.R
+++ scouting_camera.R
@@ -29,7 +29,7 @@ source("http://rcompanion.org/r_script/nagelkerke.r")
 # Working dir #
 ###############
 
-setwd("D:/Dropbox/Other/cameratrap/data")
+setwd("D:/Dropbox/Other/cameratrap/")
 
 theme_custom = theme( plot.background = element_blank(),
                       panel.background = element_blank(),
@@ -52,43 +52,22 @@ theme_custom = theme( plot.background = element_blank(),
 ################
 
 # Import data
-camera_data_raw = read.csv("cameratrapdata_14Oct15.csv")
+camera_data_raw = read.csv("data/cameratrapdata_14Oct15.csv")
 
 camera_data_clean = camera_data_raw %>%
                     select(-X2012.Date.month:-X2014.Date.month) %>%
-                    filter(is.finite(Activity..number.of.hits.)) 
+                    filter(is.finite(Activity..number.of.hits.)) %>%
+                    mutate(Middle.Date = as.Date(Middle.Date, format = "%d/%m/%Y"),
+                           Apex_predator = Dingo, 
+                           Meso_predators = Cat + Fox + Goanna,
+                           Large_mammals = Echidna + Brush.tailed.possum + Paddymelon + KOALA + Ring.tailed.possum + Swamp.wallaby,
+                           Small_mammals = short.white.tailed.mouse + Small.bat + Long.nosed.bandicoot + Anechinus + Hopping.mouse + Rat.with.long.whitish.rigid.tail,
+                           Birds = Rufous.fantail + Catbird + Lewins.honeyeater + Whites.thrush + Brush.turkey + Noisy.pitta + Lyre.bird + Yellow.throated.srubwren + Wonga.pigeon + Whipbird + Grey.shrike.thrush + Red.browed.firetail + Magpie + yellow.robin + Emerald.dove,
+                           Invertebrates = Moth + leech + Crayfish... + Spider,
+                           Reptiles = Forest.dragon + Land.mullet + Python + Goanna) %>%
+                    gather(variable, value, -Middle.Date:-Cumulative.number.of.taxa)
 
-
-
-
-camera_data = ddply(camera_data_raw, "Middle.Date", summarise, 
-                    Apex_predator = Dingo,
-                    Meso_predators = Cat + Fox + Goanna,
-                    Large_mammals = Echidna + Brush.tailed.possum + Paddymelon + KOALA + Ring.tailed.possum + Swamp.wallaby,
-                    Small_mammals = short.white.tailed.mouse + Small.bat + Long.nosed.bandicoot + Anechinus + Hopping.mouse + Rat.with.long.whitish.rigid.tail,
-                    Birds =  Rufous.fantail + Catbird + Lewins.honeyeater + Whites.thrush + Brush.turkey + Noisy.pitta + Lyre.bird + Yellow.throated.srubwren + Wonga.pigeon + Whipbird + Grey.shrike.thrush + Red.browed.firetail + Magpie + yellow.robin + Emerald.dove,
-                    Invertebrates = Moth + leech + Crayfish... + Spider,
-                    Reptiles = Forest.dragon + Land.mullet + Python + Goanna)
-
-camera_data = camera_data[complete.cases(camera_data),]
-
-species = c("Apex_predator", "Meso_predators", "Large_mammals", "Small_mammals", "Birds", "Reptiles")
-
-species = c("Dingo", 
-  "Fox", 
-  "Cat", 
-  "Long.nosed.bandicoot",
-  "Brush.tailed.possum",
-  "Paddymelon",
-#   "KOALA",
-  "Brush.turkey")
-#   "Noisy.pitta",
-#   "Lyre.bird"
-#   "Yellow.throated.srubwren")
-
-subset_data = melt(camera_data, id.vars="Middle.Date", measure.vars=species)
-
-# subset_data = rbind(subset_data, extemp_data)
+# Seasonal rects for plotting timeseries
 rect <- data.frame (xmin="2012-06-01", xmax="2012-08-31", ymin=-Inf, ymax=Inf)
 rect2 <- data.frame (xmin="2013-06-01", xmax="2013-08-31", ymin=-Inf, ymax=Inf)
 rect3 <- data.frame (xmin="2014-06-01", xmax="2014-08-31", ymin=-Inf, ymax=Inf)
@@ -99,59 +78,44 @@ rect[[1]] = as.Date(rect[[1]])
 rect[[2]] = as.Date(rect[[2]])
 
 
-# require(ggplot2)
-# require(proto)
-# StatRollApplyR <- proto(ggplot2:::Stat, { 
-#   required_aes <- c("x", "y")
-#   default_geom <- function(.) GeomLine
-#   objname <- "rollapplyr"
-#   calculate_groups <- function(., data, scales, ...) { 
-#     .super$calculate_groups(., data, scales, ...)
-#   } 
-#   calculate <- function(., data, scales, width, FUN, fill=NA, ...) {
-#     require(zoo)
-#     filtered <- rollapplyr(data$y, width, FUN, fill=fill, ...)
-#     result <- data.frame(x=data$x, y=filtered)
-#     return(result)
-#   }
-# }) 
-# stat_rollapplyr <- StatRollApplyR$new 
-# 
-# 
-# 
-# library(zoo)
-# #Make zoo object of data
-# temp.zoo<-zoo(p29$ambtemp,p29$dt)
-# 
-# #Calculate moving average with window 3 and make first and last value as NA (to ensure identical length of vectors)
-# m.av<-rollmean(temp.zoo, 3,fill = list(NA, NULL, NA))
-# 
-# #Add calculated moving averages to existing data frame
-# p29$amb.av=coredata(m.av)
+###################
+# Subset and plot #
+###################
 
+subset_data = filter(camera_data_clean, variable %in% c("Apex_predator", 
+                                                        "Meso_predators", 
+                                                        "Large_mammals", 
+                                                        "Small_mammals", 
+                                                        "Birds", 
+                                                        "Reptiles")) 
 
 ggplot() + 
   geom_rect(data=rect, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax), color=NA, alpha=0.1, inherit.aes = FALSE) +
-#   geom_smooth(data= rain_data, aes(y=value, x=Middle.Date), span = 0.3, level = 0.5, fill = NA) +
-#   geom_smooth(data= maxtemp_data, aes(y=value, x=Middle.Date), span = 0.3, level = 0.99, fill = NA) +
-#   stat_rollapplyr(data=subset_data, aes(y=value, x=Middle.Date, colour = variable), FUN = "mean", width = 10) + 
   geom_histogram(data=subset_data, aes(y=..count.., weight = value, x=Middle.Date, fill=variable),binwidth=4, alpha=0.8, position="identity")+
   geom_density(data=subset_data, aes(y=..count../4, weight = value, x=Middle.Date, fill=variable),alpha=.2, adjust=0.2) +
   facet_wrap(~variable,ncol=1, scales = "free_y") + 
   theme_custom + 
-#   theme_bw() +
   guides(fill=FALSE) + 
   scale_x_date(expand=c(0.0, 0.0)) + 
   scale_y_continuous(expand=c(0.0, 0.0)) + xlab("Date") + ylab("")
 
 
+subset_data = filter(camera_data_clean, variable %in% c("Dingo", 
+                                                        "Fox", 
+                                                        "Cat", 
+                                                        "Long.nosed.bandicoot",
+                                                        "Brush.tailed.possum",
+                                                        "Paddymelon",
+                                                        # "KOALA",
+                                                        "Brush.turkey")
+                                                        # "Noisy.pitta",
+                                                        # "Lyre.bird"
+                                                        # "Yellow.throated.srubwren")) 
 
-
-ggplot(data=subset_data, aes(x=Middle.Date)) + 
-#   geom_rect(data=rect, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax), color=NA, alpha=0.1, inherit.aes = FALSE) +
-#   geom_histogram(data=subset_data, aes(y=..count.., weight = value, x=Middle.Date, fill=variable),binwidth=4, alpha=0.8, position="identity")+
-  stat_bkde(aes(y=..count.., fill=variable, weight=value))+
-#   geom_density(data=subset_data, aes(y=..count../4, weight = value, x=Middle.Date, fill=variable),alpha=.2, adjust=0.2) +
+ggplot() + 
+  geom_rect(data=rect, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax), color=NA, alpha=0.1, inherit.aes = FALSE) +
+  geom_histogram(data=subset_data, aes(y=..count.., weight = value, x=Middle.Date, fill=variable),binwidth=4, alpha=0.8, position="identity")+
+  geom_density(data=subset_data, aes(y=..count../4, weight = value, x=Middle.Date, fill=variable),alpha=.2, adjust=0.2) +
   facet_wrap(~variable,ncol=1, scales = "free_y") + 
   theme_custom + 
   guides(fill=FALSE) + 
@@ -159,78 +123,62 @@ ggplot(data=subset_data, aes(x=Middle.Date)) +
   scale_y_continuous(expand=c(0.0, 0.0)) + xlab("Date") + ylab("")
 
 
+#########
+# Other #
+#########
 
-library(ggplot2)
-library(gridExtra)
-library(ggalt)
-
-# current verison
-packageVersion("ggalt")
-#> [1] '0.1.1.9000'
-
-data(geyser, package="MASS")
-
-ggplot(geyser, aes(x=duration)) + 
-  stat_bkde()
-
-
-
-
-
-
-
-# Small mammals, meso preds
-data_smallmam_meso = camera_data[camera_data$Small_mammals > 0 | camera_data$Meso_predators > 0,]
-smallmam_meso = glm(Small_mammals~Meso_predators, family="poisson", data=data_smallmam_meso); summary(smallmam_meso)
-data_smallmam_meso2 <- aggregate(data_smallmam_meso$Meso_predators,by=list(x=data_smallmam_meso$Meso_predators,y=data_smallmam_meso$Small_mammals),length)
-names(data_smallmam_meso2)[3] <- "count"
-
-smallmam_meso_r2 = nagelkerke(smallmam_meso)$Pseudo.R.squared.for.model.vs.null[3]
-ggplot(data=data_smallmam_meso2, aes(x=x, y=y)) +
-  geom_point(aes(size=count)) + stat_smooth(method = "glm", se = FALSE, family="poisson", colour = "red") + 
-  scale_size_continuous(range = c(2,12)) + theme_custom + 
-  ggtitle(paste("R-squared =", round(smallmam_meso_r2,2), "\nP =", signif(coef(summary(smallmam_meso))[2,4],2)))
-
-
-# Meso preds, apex preds
-data_meso_apex = camera_data[camera_data$Meso_predators > 0 | camera_data$Apex_predator > 0,]
-meso_apex = glm(Meso_predators~Apex_predator, family="poisson", data=data_meso_apex); summary(meso_apex)
-data_meso_apex2 <- aggregate(data_meso_apex$Apex_predator,by=list(x=data_meso_apex$Apex_predator,y=data_meso_apex$Meso_predators),length)
-names(data_meso_apex2)[3] <- "count"
-
-meso_apex_r2 = nagelkerke(meso_apex)$Pseudo.R.squared.for.model.vs.null[3]
-ggplot(data=data_meso_apex2, aes(x=x, y=y)) +
-  geom_point(aes(size=count)) + stat_smooth(method = "glm", se = FALSE, family="poisson", colour = "red") + 
-  scale_size_continuous(range = c(2,12)) + theme_custom + 
-  ggtitle(paste("R-squared =", round(meso_apex_r2,2), "\nP =", signif(coef(summary(meso_apex))[2,4],2)))
-
-
-# Small mammals, apex pred
-data_smallmam_apex = camera_data[camera_data$Small_mammals > 0 | camera_data$Apex_predator > 0,]
-smallmam_apex = glm(Small_mammals~Apex_predator, family="poisson", data=data_smallmam_apex); summary(smallmam_apex)
-data_smallmam_apex2 <- aggregate(data_smallmam_apex$Apex_predator,by=list(x=data_smallmam_apex$Apex_predator,y=data_smallmam_apex$Small_mammals),length)
-names(data_smallmam_apex2)[3] <- "count"
-
-smallmam_apex_r2 = nagelkerke(smallmam_apex)$Pseudo.R.squared.for.model.vs.null[3]
-ggplot(data=data_smallmam_apex2, aes(x=x, y=y)) +
-  geom_point(aes(size=count)) + stat_smooth(method = "glm", se = FALSE, family="poisson", colour = "red") + 
-  scale_size_continuous(range = c(2,12)) + theme_custom + 
-  ggtitle(paste("R-squared =", round(smallmam_apex_r2,2), "\nP =", signif(coef(summary(smallmam_apex))[2,4],2)))
-
-
-# Large mammals, birds
-data_largemam_bird = camera_data[camera_data$Large_mammals > 0 | camera_data$Birds > 0,]
-largemam_bird = glm(Large_mammals~Birds, family="poisson", data=data_largemam_bird); summary(largemam_bird)
-data_largemam_bird2 <- aggregate(data_largemam_bird$Birds,by=list(x=data_largemam_bird$Birds,y=data_largemam_bird$Large_mammals),length)
-names(data_largemam_bird2)[3] <- "count"
-
-largemam_bird_r2 = nagelkerke(largemam_bird)$Pseudo.R.squared.for.model.vs.null[3]
-ggplot(data=data_largemam_bird2, aes(x=x, y=y)) +
-  geom_point(aes(size=count)) + stat_smooth(method = "glm", se = FALSE, family="poisson", colour = "red") + 
-  scale_size_continuous(range = c(2,12)) + theme_custom + 
-  ggtitle(paste("R-squared =", round(largemam_bird_r2,2), "\nP =", signif(coef(summary(largemam_bird))[2,4],2)))
-
+# # Small mammals, meso preds
+# data_smallmam_meso = camera_data[camera_data$Small_mammals > 0 | camera_data$Meso_predators > 0,]
+# smallmam_meso = glm(Small_mammals~Meso_predators, family="poisson", data=data_smallmam_meso); summary(smallmam_meso)
+# data_smallmam_meso2 <- aggregate(data_smallmam_meso$Meso_predators,by=list(x=data_smallmam_meso$Meso_predators,y=data_smallmam_meso$Small_mammals),length)
+# names(data_smallmam_meso2)[3] <- "count"
 # 
+# smallmam_meso_r2 = nagelkerke(smallmam_meso)$Pseudo.R.squared.for.model.vs.null[3]
+# ggplot(data=data_smallmam_meso2, aes(x=x, y=y)) +
+#   geom_point(aes(size=count)) + stat_smooth(method = "glm", se = FALSE, family="poisson", colour = "red") + 
+#   scale_size_continuous(range = c(2,12)) + theme_custom + 
+#   ggtitle(paste("R-squared =", round(smallmam_meso_r2,2), "\nP =", signif(coef(summary(smallmam_meso))[2,4],2)))
+# 
+# 
+# # Meso preds, apex preds
+# data_meso_apex = camera_data[camera_data$Meso_predators > 0 | camera_data$Apex_predator > 0,]
+# meso_apex = glm(Meso_predators~Apex_predator, family="poisson", data=data_meso_apex); summary(meso_apex)
+# data_meso_apex2 <- aggregate(data_meso_apex$Apex_predator,by=list(x=data_meso_apex$Apex_predator,y=data_meso_apex$Meso_predators),length)
+# names(data_meso_apex2)[3] <- "count"
+# 
+# meso_apex_r2 = nagelkerke(meso_apex)$Pseudo.R.squared.for.model.vs.null[3]
+# ggplot(data=data_meso_apex2, aes(x=x, y=y)) +
+#   geom_point(aes(size=count)) + stat_smooth(method = "glm", se = FALSE, family="poisson", colour = "red") + 
+#   scale_size_continuous(range = c(2,12)) + theme_custom + 
+#   ggtitle(paste("R-squared =", round(meso_apex_r2,2), "\nP =", signif(coef(summary(meso_apex))[2,4],2)))
+# 
+# 
+# # Small mammals, apex pred
+# data_smallmam_apex = camera_data[camera_data$Small_mammals > 0 | camera_data$Apex_predator > 0,]
+# smallmam_apex = glm(Small_mammals~Apex_predator, family="poisson", data=data_smallmam_apex); summary(smallmam_apex)
+# data_smallmam_apex2 <- aggregate(data_smallmam_apex$Apex_predator,by=list(x=data_smallmam_apex$Apex_predator,y=data_smallmam_apex$Small_mammals),length)
+# names(data_smallmam_apex2)[3] <- "count"
+# 
+# smallmam_apex_r2 = nagelkerke(smallmam_apex)$Pseudo.R.squared.for.model.vs.null[3]
+# ggplot(data=data_smallmam_apex2, aes(x=x, y=y)) +
+#   geom_point(aes(size=count)) + stat_smooth(method = "glm", se = FALSE, family="poisson", colour = "red") + 
+#   scale_size_continuous(range = c(2,12)) + theme_custom + 
+#   ggtitle(paste("R-squared =", round(smallmam_apex_r2,2), "\nP =", signif(coef(summary(smallmam_apex))[2,4],2)))
+# 
+# 
+# # Large mammals, birds
+# data_largemam_bird = camera_data[camera_data$Large_mammals > 0 | camera_data$Birds > 0,]
+# largemam_bird = glm(Large_mammals~Birds, family="poisson", data=data_largemam_bird); summary(largemam_bird)
+# data_largemam_bird2 <- aggregate(data_largemam_bird$Birds,by=list(x=data_largemam_bird$Birds,y=data_largemam_bird$Large_mammals),length)
+# names(data_largemam_bird2)[3] <- "count"
+# 
+# largemam_bird_r2 = nagelkerke(largemam_bird)$Pseudo.R.squared.for.model.vs.null[3]
+# ggplot(data=data_largemam_bird2, aes(x=x, y=y)) +
+#   geom_point(aes(size=count)) + stat_smooth(method = "glm", se = FALSE, family="poisson", colour = "red") + 
+#   scale_size_continuous(range = c(2,12)) + theme_custom + 
+#   ggtitle(paste("R-squared =", round(largemam_bird_r2,2), "\nP =", signif(coef(summary(largemam_bird))[2,4],2)))
+
+
 # # Meso preds, apex preds
 # # data_meso_apex <- transform( camera_data, Meso_predators = sample(Meso_predators), Apex_predator = sample(Apex_predator) )
 # data_meso_apex = camera_data
@@ -244,91 +192,3 @@ ggplot(data=data_largemam_bird2, aes(x=x, y=y)) +
 #   geom_point(aes(size=count)) + stat_smooth(method = "glm", se = FALSE, family="poisson", colour = "red") + 
 #   scale_size_continuous(range = c(2,12)) + theme_custom + 
 #   ggtitle(paste("R-squared =", round(meso_apex_r2,2), "\nP =", signif(coef(summary(meso_apex))[2,4],2)))
-# 
-
-
-
-
-# Codyn
-library("tidyr")
-
-colnames(camera_data_raw)
-
-
-codyn_data = camera_data_raw[,c(1,11:50)]
-codyn_data = codyn_data[complete.cases(codyn_data),]
-
-codyn_melt = gather(codyn_data, species, abundance, -Middle.Date)
-
-codyn_melt$Middle.Date = as.numeric(codyn_melt$Middle.Date)
-
-community_stability(codyn_melt, time.var = "Middle.Date", abundance.var = "abundance")
-
-synchrony(codyn_melt, time.var = "Middle.Date", abundance.var = "abundance", metric = "Gross")
-
-# Turnover 
-
-turn = turnover(df = codyn_melt, 
-         time.var = "Middle.Date", 
-         species.var = "species", 
-         abundance.var = "abundance")
-plot(turn$Middle.Date, turn$total, type="line")
-
-turn = turnover(df = codyn_melt, 
-                time.var = "Middle.Date", 
-                species.var = "species", 
-                abundance.var = "abundance",
-                metric = "appearance")
-plot(turn$Middle.Date, turn$appearance, type="line")
-
-turn = turnover(df = codyn_melt, 
-                time.var = "Middle.Date", 
-                species.var = "species", 
-                abundance.var = "abundance",
-                metric = "disappearance")
-plot(turn$Middle.Date, turn$disappearance, type="line")
-
-
-# Rank shift
-
-rankshift <- mean_rank_shift(df=codyn_melt, 
-                             time.var = "Middle.Date",  
-                             abundance.var = "abundance")
-
-plot(rownames(rankshift), rankshift$MRS, type="line")
-
-
-comm.res <- rate_change_interval(df=codyn_melt, 
-                                 time.var = "Middle.Date", 
-                                 species.var = "species",
-                                 abundance.var = "abundance")
-
-ggplot(comm.res, aes(interval, distance)) + 
-  geom_point() + theme_bw() + stat_smooth(method = "lm", se = F, size = 2)
-
-
-
-
-
-
-
-
-
-rate_change(codyn_melt, time.var = "Middle.Date", species.var = "species",
-            abundance.var = "abundance", replicate.var = NA)
-
-x = rate_change_interval(codyn_melt, time.var = "Middle.Date", species.var = "species",
-                     abundance.var = "abundance", replicate.var = NA)
-
-
-synch_onerep(codyn_melt, time.var = "Middle.Date", species.var = "species", abundance.var = "abundance", metric = c("Loreau"))
-
-turnover_allyears(codyn_melt, time.var = "Middle.Date", species.var, abundance.var,
-                  metric = c("total", "disappearance", "appearance"))
-
-
-require(pscl)
-
-summary(zeroinfl(Meso_predators~Apex_predator, data=data_meso_apex))
-
-
